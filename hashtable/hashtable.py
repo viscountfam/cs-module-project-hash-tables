c511a7ce82f46a761625080c44cd78d2a3231342
Michael Famurewa <myfamurewa@gmail.com>
2020-08-04 17:04:28 -0400

    everything ready except resize

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -2,14 +2,181 @@ class HashTableEntry:
     """
     Linked List hash table key/value pair
     """
-    def __init__(self, key, value):
+    def __init__(self, key, value, prev=None, next_up=None):
         self.key = key
         self.value = value
-        self.next = None
-
+        self.next = next_up
+        self.prev = prev
+    def delete(self):
+        if self.prev:
+            self.prev.next = self.next
+        if self.next:
+            self.next.prev = self.prev
 
 # Hash table can't have fewer than this many slots
 MIN_CAPACITY = 8
+
+class DoublyLinkedList:
+    def __init__(self, node=None):
+        self.head = node
+        self.tail = node
+        self.length = 1 if node is not None else 0
+    
+    def __len__(self):
+        return self.length
+    
+    """
+    Wraps the given value in a ListNode and inserts it as the new head of the list.
+    Don't forget to handle the old head node's previous pointer accordingly.
+
+    """
+
+    def add_to_head(self, key, value):
+        # create instance of Listnode with a value
+        new_list_node = HashTableEntry(key, value)
+        # increment the DLL length attribute
+        self.length += 1
+        
+        #check if DLL is empty
+            # if so set head and tail to the new node instance
+
+        # if DLL is not empty
+        if self.head is not None:
+                # set new node's next to current head
+            new_list_node.next = self.head
+                # set head's prev to new node
+            self.head.prev = new_list_node
+                # set head to the new node
+            self.head = new_list_node
+        else:
+            self.head = new_list_node
+            self.tail = new_list_node
+
+    def add_to_tail(self, key, value):
+        # create instance of ListNode with a value
+        new_list_node = HashTableEntry(value)
+        # increment the DLL length attribute
+        self.length += 1
+        # check to see is DLL is empty
+        if self.head is not None:
+            # Assign the newly created node to the current tail's next
+            self.tail.next = new_list_node
+            # Assign the current tail to the newly created node's prev
+            new_list_node.prev = self.tail
+            # Assign self.tail to the newly created node
+            self.tail = new_list_node
+        else:
+            self.head = new_list_node
+            self.tail = new_list_node
+
+    def remove_from_head(self):
+        if self.head:
+            value = self.head.value
+        else:
+            value = None
+        self.delete(self.head)
+        return value
+
+    def remove_from_tail(self):
+        if self.tail:
+            value = self.tail.value
+        else: 
+            value = None
+        self.delete(self.tail)
+        return value
+
+    """
+    Removes the input node from its current spot in the List and inserts is as the new head node of the List.
+    """
+    def move_to_front(self, node):
+        self.delete(node)
+        self.add_to_head(node.value)
+
+
+    """
+    Remove the input node from its current spot in the List and inserts it as the new tail node of the List.
+    """
+    def move_to_end(self, node):
+        self.delete(node)
+        self.add_to_tail(node.value)
+
+    """
+    Deletes the input node from the List, preserving the order of the other elements of the List.
+    """
+    def delete(self, node):
+        # the logic here should be the same as move to end and move to start without the head and tail adjustments
+        # if empty
+        if not self.head and not self.tail:
+            return None
+        #if head and tail
+        if node == self.head and self.head == self.tail:
+            self.head = None
+            self.tail = None
+        elif node == self.head:
+            self.head = self.head.next
+            node.delete()
+        elif node == self.tail:
+            self.tail = self.tail.prev
+            node.delete()
+        else:
+            node.delete()
+
+        self.length -= 1
+
+
+    """
+    Finds and returns the maximum value of all the nodes in the list.
+
+    """
+    def get_max(self):
+        maximum = 0
+        current = self.head
+        while current is not None:
+            if current.value > maximum:
+                maximum = current.value
+            current = current.next
+        return maximum
+
+
+    def find(self, key):
+        current = self.head
+
+        while current is not None:
+            if current.key == key:
+                return current
+            current = current.next
+        
+        return current
+    
+    def update_or_insert_at_head(self, key, value):
+        current = self.head
+        while current is not None:
+            if current.key == key:
+                current.value = value
+                return
+            current = current.next
+        
+        self.add_to_head(key, value)
+    
+    def update_or_insert_at_tail(self, key, value):
+        current = self.head
+        while current is not None:
+            if current.key == key:
+                current.value = value
+                return
+            current = current.next
+        
+        self.add_to_tail(key, value)
+
+    def find_and_delete(self, key):
+        current = self.head
+
+        while current is not None:
+            if current.key == key:
+                self.delete(current)
+        
+        return None
+
 
 
 class HashTable:
@@ -105,7 +272,14 @@ class HashTable:
         Implement this.
         """
         # Your code here
-        self.table[self.hash_index(key)] = key, value
+        if self.table[self.hash_index(key)] is None:
+            self.table[self.hash_index(key)] = DoublyLinkedList(HashTableEntry(key, value))
+        else:
+            if self.table[self.hash_index(key)].find(key):
+                self.table[self.hash_index(key)].update_or_insert_at_tail(key, value)
+            else:
+                self.table[self.hash_index(key)].add_to_tail(key,value)
+
 
 
     def delete(self, key):
@@ -119,9 +293,12 @@ class HashTable:
         # Your code here
         
         if self.table[self.hash_index(key)]:
-            deleted = self.table[self.hash_index(key)]
-            self.table[self.hash_index(key)] = None
-            return deleted
+            if self.table[self.hash_index(key)].head.key == key:
+                self.table[self.hash_index(key)].delete(self.table[self.hash_index(key)].head)
+            elif self.table[self.hash_index(key)].tail.key == key:
+                self.table[self.hash_index(key)].delete(self.table[self.hash_index(key)].tail)
+            else:
+                self.table[self.hash_index(key)].find_and_delete(key)
         else:
             print("There is no value stored here")
 
@@ -136,8 +313,12 @@ class HashTable:
         """
         # Your code here
         if self.table[self.hash_index(key)]:
-            print(self.table[self.hash_index(key)][1])
-            return self.table[self.hash_index(key)][1]
+            if self.table[self.hash_index(key)].head == key:
+                return self.table[self.hash_index(key)].head
+            elif self.table[self.hash_index(key)].tail == key:
+                return self.table[self.hash_index(key)].tail
+            else:
+                self.table[self.hash_index(key)].find(key)
         else:
             return None
 
@@ -151,13 +332,17 @@ class HashTable:
         """
         # Your code here
         # create a new table
-        newTable = [None for i in range(self.capacity * 2)]
+        newTable = [None for i in range(self.capacity)]
         # loop through the old table
         for element in self.table:
             # if the array is populated
             if element:
                 # loop through those values and rehash them
-                    newTable[self.hash_index(element[0])] = element[0], element[1]
+                    current = element.head
+                    while current is not None:
+                        newTable.put(element.key, element.value)
+                        current = current.next
+                    
                 
         self.table = newTable
         self.capacity = self.capacity * 2
